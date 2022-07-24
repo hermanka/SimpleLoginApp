fb51ac60a39c6dcfe57c2c65c027cdb7a7c5ff4d
hermanka <hermanka.beta@gmail.com>
2022-07-24 10:09:45 +0700

    2nd commit

--- src/main/java/com/sqliteloginapp/Main.java
+++ src/main/java/com/sqliteloginapp/Main.java
@@ -15,27 +15,29 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {			
-		Connection c = null;                
-                Class.forName("org.sqlite.JDBC");
-                c = DriverManager.getConnection("jdbc:sqlite:accounts.db");
-                c.setAutoCommit(false);
+		
+            Connection c = null;                
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:accounts.db");
+            c.setAutoCommit(false);
 
-                Statement stmt = null;
-                Scanner u = new Scanner(System.in);
-                System.out.println("Enter username");
-                String userName = u.nextLine();              
-                Scanner p = new Scanner(System.in); 
-                System.out.println("Enter password");
-                String passWord = p.nextLine();        
+            Statement stmt = null;
+            Scanner u = new Scanner(System.in);
+            System.out.println("Enter username");
+            String userName = u.nextLine();              
+            Scanner p = new Scanner(System.in); 
+            System.out.println("Enter password");
+            String passWord = p.nextLine();        
+
+            String query = "select * from user where username='" + userName + "' and password='" + passWord + "'";
+            stmt = c.createStatement();
+
+            ResultSet res = stmt.executeQuery(query);
+            if(res.next()){
+                System.out.println("Success login, welcome "+ userName);
+            } else {
+                System.out.println("Failed login");
+            }
                 
-                String query = "select * from user where username='" + userName + "' and password='" + passWord + "'";
-                stmt = c.createStatement();
-                
-                ResultSet res = stmt.executeQuery(query);
-                if(res.next()){
-                    System.out.println("Success login");
-                } else {
-                    System.out.println("Failed login");
-                }                         
 	}     
 }
